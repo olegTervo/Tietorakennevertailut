c1dc8a6634072d47608110bc4c225c4d193ed10a
Oleg Tervo-Ridor <oleg.tervo-ridor@helsinki.fi>
2021-03-27 15:41:35 +0200

    initial commit tests

---
+++ HakuVertailu/test/UnitTests/UnitTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UnitTests;
+
+import java.util.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author oleg
+ */
+public class UnitTest {
+    
+    List testList;
+    
+    public UnitTest() {
+        testList = new ArrayList<String>();
+    }
+    
+    @Before
+    public void setUp() {
+        for(int i = 0; i < 10000; i++) testList.add(i);
+    }
+    
+    @After
+    public void tearDown() {
+        testList = new ArrayList<String>();
+    }
+
+    @Test
+    public void test1() {
+        assertTrue(testList.indexOf(4444) == 4444);
+    }
+}
