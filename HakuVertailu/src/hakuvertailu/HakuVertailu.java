a754899cbbfaa20443d908db63c452d1d2d6e46e
Oleg Tervo-Ridor <oleg.tervo-ridor@helsinki.fi>
2021-03-27 14:48:12 +0200

    UI template

--- HakuVertailu/src/hakuvertailu/HakuVertailu.java
+++ HakuVertailu/src/hakuvertailu/HakuVertailu.java
@@ -5,17 +5,69 @@
  */
 package hakuvertailu;
 
+
+import com.sun.prism.paint.Color;
+import java.util.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 /**
  *
  * @author oleg
  */
-public class HakuVertailu {
+public class HakuVertailu extends Application {
 
+    private Stage primary;
+    private Scene mainScene;
+     
+    @Override
+    public void start(Stage primary) throws Exception {
+        
+        this.primary = primary;
+        
+        HBox titleBox = new HBox();
+        titleBox.setStyle("-fx-alignment: center;");
+        Label title = new Label("Haku Vertailu Home");
+        title.setStyle("-fx-font-size: 20px;");
+        titleBox.getChildren().add(title);
+        //title.setStyle("-fx-content-display: center;");
+        
+        TextArea console = new TextArea();
+        console.setEditable(false);
+        console.setText("Wellcoma to the Application!");
+        console.setStyle("-fx-control-inner-background: black;");
+        
+        TextField commandField = new TextField();
+        Button run = new Button("Run");
+        
+        HBox runCommandBox = new HBox();
+        runCommandBox.setSpacing(20);
+        runCommandBox.getChildren().add(commandField);
+        runCommandBox.getChildren().add(run);
+        
+        VBox mainPage = new VBox();
+        mainPage.setSpacing(10);
+        mainPage.getChildren().add(titleBox);
+        mainPage.getChildren().add(runCommandBox);
+        mainPage.getChildren().add(console);
+        
+        mainScene = new Scene(mainPage);
+        
+        primary.setTitle("Haku Vertailu");
+        primary.setScene(mainScene);
+        primary.show();
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
     
 }
